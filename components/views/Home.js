04c7dc224d45fd83d49ab6afeec9cf76951dc45d
Ashley Morgan <aglass754@gmail.com>
2024-08-15 13:21:29 -0700

    add homepage animation using gsap

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-    <div id="homePage">
+    <div class="homePage">
         <h1 id = "me">Ashley Morgan</h1>
         <h3 id = "whatIDo">Welcome! I enjoy Full Stack Web Development, Security, and solving any coding challenge</h3>
         <h4 id = "enter">Click To Enter</h4>
